333ff8fbc8193ff273650b1dfbabfdd99800aec0
minaanwer <minaanwer05@gmail.com>
2020-04-01 00:22:37 +0200

    init

---
+++ SpringBoot Rest API Connecting MSQL Server via Hibernate/demo/src/main/java/net/guides/springboot2/crud/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package net.guides.springboot2.crud.controller;
+
+
+import net.guides.springboot2.crud.model.Employee;
+import net.guides.springboot2.crud.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
---
+++ SpringBoot Rest API Connecting MSQL Server via Hibernate/demo/src/main/java/net/guides/springboot2/crud/controller/test.java
@@ -0,0 +1,17 @@
+package net.guides.springboot2.crud.controller;
+
+
+import net.guides.springboot2.crud.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class test {
+    @GetMapping("/test")
+    public String getAllEmployees() {
+
+       return  "8or yad !!";
+    }
+}
