447e6ff3ad8bf8d93024279a68bb226c2d8d3c2c
minaanwer <minaanwer05@gmail.com>
2020-04-01 00:31:45 +0200

    init

---
+++ RabbitMQ/Sender/JavaInUse-withTest/src/test/java/com/javainuse/test/EmployeeTests.java
@@ -0,0 +1,20 @@
+
+/*
+package com.javainuse.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeTests {
+
+    @test
+    public void  when_writeClassName_SerializaAllItsVariables(){
+
+    }
+
+}
+*/
