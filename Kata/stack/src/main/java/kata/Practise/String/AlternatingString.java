18a04e212c1feb1969c4143c776115e977d90a43
mina anwer <minaanwer05@gmail.com>
2020-08-30 10:37:01 +0200

    update

---
+++ Kata/stack/src/main/java/kata/Practise/String/AlternatingString.java
@@ -0,0 +1,13 @@
+package kata.Practise.String;
+
+public class AlternatingString {
+
+    public static int alternate(String test) {
+      return 0;
+
+        //Get characters dist.
+        //get all prop.
+        //
+
+    }
+}
---
+++ Kata/stack/src/test/java/kata/Practise/String/AlternatingStringTests.java
@@ -0,0 +1,24 @@
+package kata.Practise.String;
+
+
+import org.junit.Test;
+
+public class AlternatingStringTests {
+
+
+    @Test
+    public void doNothing(){
+
+    }
+
+    @Test
+    public void whenCall_AlternateFunction_ReturnAnyValue(){
+      int result=   AlternatingString.alternate("test");
+    }
+
+    @Test
+    public void  when_Call
+
+
+
+}
