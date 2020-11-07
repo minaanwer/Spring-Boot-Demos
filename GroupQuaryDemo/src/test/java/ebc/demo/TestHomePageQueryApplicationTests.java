8eba61a6afd25b77cf531b99c5e671bf38739707
mina anwer <minaanwer05@gmail.com>
2020-11-07 17:23:43 +0200

    init

---
+++ GroupQuaryDemo/src/test/java/ebc/demo/TestHomePageQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package ebc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHomePageQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
